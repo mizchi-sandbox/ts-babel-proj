2ebeebc1dd53bb587df6b4525a8ccc17e9de9772
Koutarou Chikuba <miz404@gmail.com>
2015-12-07 15:07:52 +0900

    Add

---
+++ public/bundle.js
@@ -0,0 +1,29 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, Promise, generator) {
+    return new Promise(function (resolve, reject) {
+        generator = generator.call(thisArg, _arguments);
+        function cast(value) { return value instanceof Promise && value.constructor === Promise ? value : new Promise(function (resolve) { resolve(value); }); }
+        function onfulfill(value) { try { step("next", value); } catch (e) { reject(e); } }
+        function onreject(value) { try { step("throw", value); } catch (e) { reject(e); } }
+        function step(verb, value) {
+            var result = generator[verb](value);
+            result.done ? resolve(result.value) : cast(result.value).then(onfulfill, onreject);
+        }
+        step("next", void 0);
+    });
+};
+class MyClass {
+    constructor() {
+        console.log("main");
+    }
+    a() {
+        return __awaiter(this, void 0, Promise, function* () {
+            return "";
+        });
+    }
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = MyClass;
+
+},{}]},{},[1]);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export default class MyClass {
+  constructor() {
+    console.log("main");
+  }
+
+  async a() {
+    return "";
+  }
+}
